bd311d7ad81b9a124d958b39c87a23b0d6575e8e
Chunky <jasonc@nulogy.com>
2020-08-12 17:05:07 -0400

    refactor

--- ruby/game.rb
+++ ruby/game.rb
@@ -1,10 +1,17 @@
 module UglyTrivia
   class Game
-    def  initialize
+    def initialize
       @players = []
       @places = Array.new(6, 0)
       @purses = Array.new(6, 0)
       @in_penalty_box = Array.new(6, nil)
+
+      @questions = {
+        "Pop" => 0,
+        "Science" => 0,
+        "Sports" => 0,
+        "Rock" => 0,
+      }
 
       @pop_questions = []
       @science_questions = []
@@ -51,12 +58,12 @@ module UglyTrivia
       puts "They have rolled a #{roll}"
 
       if @in_penalty_box[@current_player]
-        if roll % 2 != 0
+        if roll.odd?
           @is_getting_out_of_penalty_box = true
 
           puts "#{@players[@current_player]} is getting out of the penalty box"
-          @places[@current_player] = @places[@current_player] + roll
-          @places[@current_player] = @places[@current_player] - 12 if @places[@current_player] > 11
+          @places[@current_player] += roll
+          @places[@current_player] %= 12
 
           puts "#{@players[@current_player]}'s new location is #{@places[@current_player]}"
           puts "The category is #{current_category}"
@@ -64,12 +71,12 @@ module UglyTrivia
         else
           puts "#{@players[@current_player]} is not getting out of the penalty box"
           @is_getting_out_of_penalty_box = false
-          end
+        end
 
       else
 
-        @places[@current_player] = @places[@current_player] + roll
-        @places[@current_player] = @places[@current_player] - 12 if @places[@current_player] > 11
+        @places[@current_player] += roll
+        @places[@current_player] %= 12
 
         puts "#{@players[@current_player]}'s new location is #{@places[@current_player]}"
         puts "The category is #{current_category}"
@@ -77,38 +84,25 @@ module UglyTrivia
       end
     end
 
-  private
-
     def ask_question
-      puts @pop_questions.shift if current_category == 'Pop'
-      puts @science_questions.shift if current_category == 'Science'
-      puts @sports_questions.shift if current_category == 'Sports'
-      puts @rock_questions.shift if current_category == 'Rock'
+      puts @pop_questions.shift if current_category == "Pop"
+      puts @science_questions.shift if current_category == "Science"
+      puts @sports_questions.shift if current_category == "Sports"
+      puts @rock_questions.shift if current_category == "Rock"
     end
 
     def current_category
-      return 'Pop' if @places[@current_player] == 0
-      return 'Pop' if @places[@current_player] == 4
-      return 'Pop' if @places[@current_player] == 8
-      return 'Science' if @places[@current_player] == 1
-      return 'Science' if @places[@current_player] == 5
-      return 'Science' if @places[@current_player] == 9
-      return 'Sports' if @places[@current_player] == 2
-      return 'Sports' if @places[@current_player] == 6
-      return 'Sports' if @places[@current_player] == 10
-      return 'Rock'
+      %w[Pop Science Sports Rock][@places[@current_player] % 4]
     end
-
-  public
 
     def was_correctly_answered
       if @in_penalty_box[@current_player]
         if @is_getting_out_of_penalty_box
-          puts 'Answer was correct!!!!'
+          puts "Answer was correct!!!!"
           @purses[@current_player] += 1
           puts "#{@players[@current_player]} now has #{@purses[@current_player]} Gold Coins."
 
-          winner = did_player_win()
+          winner = did_player_win
           @current_player += 1
           @current_player = 0 if @current_player == @players.length
 
@@ -118,8 +112,6 @@ module UglyTrivia
           @current_player = 0 if @current_player == @players.length
           true
         end
-
-
 
       else
 
@@ -131,24 +123,22 @@ module UglyTrivia
         @current_player += 1
         @current_player = 0 if @current_player == @players.length
 
-        return winner
+        winner
       end
     end
 
     def wrong_answer
-  		puts 'Question was incorrectly answered'
-  		puts "#{@players[@current_player]} was sent to the penalty box"
-  		@in_penalty_box[@current_player] = true
+      puts "Question was incorrectly answered"
+      puts "#{@players[@current_player]} was sent to the penalty box"
+      @in_penalty_box[@current_player] = true
 
       @current_player += 1
       @current_player = 0 if @current_player == @players.length
-  		return true
+      true
     end
 
-  private
-
     def did_player_win
-      !(@purses[@current_player] == 6)
+      @purses[@current_player] != 6
     end
   end
 end
