3c5d886ecd0f4b4886598d9ab3aee98ca8217130
Chunky <jasonc@nulogy.com>
2020-12-14 02:09:35 -0500

    refactor

--- ruby/game.rb
+++ ruby/game.rb
@@ -10,77 +10,44 @@ module UglyTrivia
   end
 
   class Game
+    attr_reader :players, :questions
+
     def initialize
+      @players = []
       @questions = {
         "Pop" => 0,
         "Science" => 0,
         "Sports" => 0,
         "Rock" => 0,
       }
-
-      @is_getting_out_of_penalty_box = false
-      @players = []
     end
 
     def add(player_name)
-      @players << Player.new(player_name)
-
+      players << Player.new(player_name)
       puts "#{player_name} was added"
-      puts "They are player number #{@players.length}"
-
-      true
+      puts "They are player number #{players.length}"
     end
 
     def roll(roll)
       puts "#{current_player.name} is the current player"
       puts "They have rolled a #{roll}"
+      check_if_getting_out_of_penalty_box(roll)
+      return if current_player.in_penalty_box
 
-      if current_player.in_penalty_box
-        if roll.odd?
-          @is_getting_out_of_penalty_box = true
-
-          puts "#{current_player.name} is getting out of the penalty box"
-          current_player.location += roll
-          current_player.location %= 12
-
-          puts "#{current_player.name}'s new location is #{current_player.location}"
-          puts "The category is #{current_category}"
-          ask_question
-        else
-          puts "#{current_player.name} is not getting out of the penalty box"
-          @is_getting_out_of_penalty_box = false
-        end
-
-      else
-
-        current_player.location += roll
-        current_player.location %= 12
-
-        puts "#{current_player.name}'s new location is #{current_player.location}"
-        puts "The category is #{current_category}"
-        ask_question
-      end
+      current_player.location += roll
+      current_player.location %= 12
+      puts "#{current_player.name}'s new location is #{current_player.location}"
+      puts "The category is #{current_category}"
+      ask_question
     end
 
     def was_correctly_answered
-      if current_player.in_penalty_box
-        if @is_getting_out_of_penalty_box
-          puts "Answer was correct!!!!"
-          current_player.purse += 1
-          puts "#{current_player.name} now has #{current_player.purse} Gold Coins."
-
-          goto_next_player
-        else
-          goto_next_player
-        end
-      else
-
+      unless current_player.in_penalty_box
         puts "Answer was corrent!!!!"
         current_player.purse += 1
         puts "#{current_player.name} now has #{current_player.purse} Gold Coins."
-
-        goto_next_player
       end
+      goto_next_player
       !winner?
     end
 
@@ -88,32 +55,42 @@ module UglyTrivia
       puts "Question was incorrectly answered"
       puts "#{current_player.name} was sent to the penalty box"
       current_player.in_penalty_box = true
-
       goto_next_player
       !winner?
     end
 
     private
 
+    def check_if_getting_out_of_penalty_box(roll)
+      return unless current_player.in_penalty_box
+
+      if roll.odd?
+        current_player.in_penalty_box = false
+        puts "#{current_player.name} is getting out of the penalty box"
+      else
+        puts "#{current_player.name} is not getting out of the penalty box"
+      end
+    end
+
     def goto_next_player
-      @players.rotate!
+      players.rotate!
     end
 
     def current_player
-      @players.first
+      players.first
     end
 
     def ask_question
-      puts "#{current_category} Question #{@questions[current_category]}"
-      @questions[current_category] += 1
+      puts "#{current_category} Question #{questions[current_category]}"
+      questions[current_category] += 1
     end
 
     def current_category
-      @questions.keys[current_player.location % @questions.keys.size]
+      questions.keys[current_player.location % questions.keys.size]
     end
 
     def winner?
-      @players.any?(&:winner?)
+      players.any?(&:winner?)
     end
   end
 end
