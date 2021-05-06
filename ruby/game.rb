6cf5bc074d518f27b18a9a6252e99b2a2cda62a1
Chunky <jasonc@nulogy.com>
2021-05-06 07:38:58 -0400

    refactor

--- ruby/game.rb
+++ ruby/game.rb
@@ -1,149 +1,90 @@
 module UglyTrivia
+  class Player < Struct.new(:name, :place, :purse, :in_penalty_box)
+    def initialize(name)
+      super(name, 0, 0, false)
+    end
+
+    def winner?
+      purse >= 6
+    end
+  end
+
   class Game
+    CATEGORIES = %w[Pop Science Sports Rock]
+
+    attr_reader :players
+
     def initialize
       @players = []
-      @places = Array.new(6, 0)
-      @purses = Array.new(6, 0)
-      @in_penalty_box = Array.new(6, nil)
-
-      @pop_questions = []
-      @science_questions = []
-      @sports_questions = []
-      @rock_questions = []
-
-      @current_player = 0
-      @is_getting_out_of_penalty_box = false
-
-      50.times do |i|
-        @pop_questions.push "Pop Question #{i}"
-        @science_questions.push "Science Question #{i}"
-        @sports_questions.push "Sports Question #{i}"
-        @rock_questions.push create_rock_question(i)
-      end
+      @questions = CATEGORIES.map { |category| [category, 0] }.to_h
     end
 
     def add(player_name)
-      @players.push player_name
-      @places[how_many_players] = 0
-      @purses[how_many_players] = 0
-      @in_penalty_box[how_many_players] = false
-
+      players << Player.new(player_name)
       puts "#{player_name} was added"
-      puts "They are player number #{@players.length}"
-
-      true
+      puts "They are player number #{players.count}"
     end
 
     def roll(roll)
-      puts "#{@players[@current_player]} is the current player"
+      puts "#{current_player.name} is the current player"
       puts "They have rolled a #{roll}"
+      check_whether_player_exiting_penalty_box(roll)
+      return if current_player.in_penalty_box
 
-      if @in_penalty_box[@current_player]
-        if roll.odd?
-          @is_getting_out_of_penalty_box = true
-
-          puts "#{@players[@current_player]} is getting out of the penalty box"
-          @places[@current_player] = @places[@current_player] + roll
-          @places[@current_player] = @places[@current_player] - 12 if @places[@current_player] > 11
-
-          puts "#{@players[@current_player]}'s new location is #{@places[@current_player]}"
-          puts "The category is #{current_category}"
-          ask_question
-        else
-          puts "#{@players[@current_player]} is not getting out of the penalty box"
-          @is_getting_out_of_penalty_box = false
-        end
-
-      else
-
-        @places[@current_player] = @places[@current_player] + roll
-        @places[@current_player] = @places[@current_player] - 12 if @places[@current_player] > 11
-
-        puts "#{@players[@current_player]}'s new location is #{@places[@current_player]}"
-        puts "The category is #{current_category}"
-        ask_question
-      end
+      current_player.place += roll
+      current_player.place %= 12
+      puts "#{current_player.name}'s new location is #{current_player.place}"
+      puts "The category is #{current_category}"
+      question_number = @questions[current_category]
+      puts "#{current_category} Question #{question_number}"
+      @questions[current_category] += 1
     end
 
     def was_correctly_answered
-      if @in_penalty_box[@current_player]
-        if @is_getting_out_of_penalty_box
-          puts "Answer was correct!!!!"
-          @purses[@current_player] += 1
-          puts "#{@players[@current_player]} now has #{@purses[@current_player]} Gold Coins."
-
-          winner = did_player_win
-          @current_player += 1
-          @current_player = 0 if @current_player == @players.length
-
-          winner
-        else
-          @current_player += 1
-          @current_player = 0 if @current_player == @players.length
-          true
-        end
-
-      else
-
-        puts "Answer was corrent!!!!"
-        @purses[@current_player] += 1
-        puts "#{@players[@current_player]} now has #{@purses[@current_player]} Gold Coins."
-
-        winner = did_player_win
-        @current_player += 1
-        @current_player = 0 if @current_player == @players.length
-
-        winner
+      unless current_player.in_penalty_box
+        puts "Answer was correct!!!!"
+        current_player.purse += 1
+        puts "#{current_player.name} now has #{current_player.purse} Gold Coins."
       end
+      advance_to_next_player
+      !game_over?
     end
 
     def wrong_answer
       puts "Question was incorrectly answered"
-      puts "#{@players[@current_player]} was sent to the penalty box"
-      @in_penalty_box[@current_player] = true
-
-      @current_player += 1
-      @current_player = 0 if @current_player == @players.length
-      true
+      puts "#{current_player.name} was sent to the penalty box"
+      current_player.in_penalty_box = true
+      advance_to_next_player
+      !game_over?
     end
 
     private
 
-    def did_player_win
-      @purses[@current_player] != 6
+    def check_whether_player_exiting_penalty_box(roll)
+      return unless current_player.in_penalty_box
+
+      if roll.odd?
+        current_player.in_penalty_box = false
+        puts "#{current_player.name} is getting out of the penalty box"
+      else
+        puts "#{current_player.name} is not getting out of the penalty box"
+      end
     end
 
-    def create_rock_question(index)
-      "Rock Question #{index}"
+    def advance_to_next_player
+      players.rotate!
     end
 
-    def is_playable?
-      how_many_players >= 2
+    def current_player
+      players.first
     end
 
-    def how_many_players
-      @players.length
-    end
-
-    def ask_question
-      puts @pop_questions.shift if current_category == "Pop"
-      puts @science_questions.shift if current_category == "Science"
-      puts @sports_questions.shift if current_category == "Sports"
-      puts @rock_questions.shift if current_category == "Rock"
+    def game_over?
+      players.any?(:winner?)
     end
 
     def current_category
-      return "Pop" if @places[@current_player] == 0
-      return "Pop" if @places[@current_player] == 4
-      return "Pop" if @places[@current_player] == 8
-      return "Science" if @places[@current_player] == 1
-      return "Science" if @places[@current_player] == 5
-      return "Science" if @places[@current_player] == 9
-      return "Sports" if @places[@current_player] == 2
-      return "Sports" if @places[@current_player] == 6
-      return "Sports" if @places[@current_player] == 10
-
-      "Rock"
+      CATEGORIES[current_player.place % CATEGORIES.count]
     end
   end
 end
