e7ee7903a471b1a6822f50a93688845eca5e6d31
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-02 18:36:12 -0400

    refactor

--- ruby/game.rb
+++ ruby/game.rb
@@ -4,20 +4,45 @@ module UglyTrivia
       super(name, 0, 0, false)
     end
 
+    def enter_penalty_box
+      self.in_penalty_box = true
+    end
+
+    def leave_penalty_box
+      self.in_penalty_box = false
+    end
+
+    def gets_money
+      self.purse += 1
+    end
+
     def winner?
       purse >= 6
     end
   end
 
-  class Game
-    QUESTION_CATEGORIES = %w[Pop Science Sports Rock]
+  class Questions
+    CATEGORIES = %w[Pop Science Sports Rock]
 
+    attr_reader :questions
+
+    def initialize
+      @questions = Hash.new(0)
+    end
+
+    def next_question(category)
+      result = "#{category} Question #{questions[category]}"
+      questions[category] += 1
+      result
+    end
+  end
+
+  class Game
     attr_reader :players, :questions
 
     def initialize
       @players = []
-      @questions = Hash.new(0)
-      @is_getting_out_of_penalty_box = false
+      @questions = Questions.new
     end
 
     def add(player_name)
@@ -29,68 +54,57 @@ module UglyTrivia
     def roll(roll)
       puts "#{current_player.name} is the current player"
       puts "They have rolled a #{roll}"
-      if current_player.in_penalty_box
-        if roll.odd?
-          @is_getting_out_of_penalty_box = true
-          puts "#{current_player.name} is getting out of the penalty box"
-          current_player.place += roll
-          current_player.place %= 12
-          puts "#{current_player.name}'s new location is #{current_player.place}"
-          puts "The category is #{current_category}"
-          ask_question
-        else
-          puts "#{current_player.name} is not getting out of the penalty box"
-          @is_getting_out_of_penalty_box = false
-        end
-      else
-        current_player.place += roll
-        current_player.place %= 12
-        puts "#{current_player.name}'s new location is #{current_player.place}"
-        puts "The category is #{current_category}"
-        ask_question
-      end
+      check_if_exiting_penalty_box(roll)
+      return if current_player.in_penalty_box
+
+      current_player.place += roll
+      current_player.place %= 12
+      puts "#{current_player.name}'s new location is #{current_player.place}"
+      puts "The category is #{current_category}"
+      ask_question
     end
 
     def was_correctly_answered
-      if current_player.in_penalty_box
-        if @is_getting_out_of_penalty_box
-          puts "Answer was correct!!!!"
-          current_player.purse += 1
-          puts "#{current_player.name} now has #{current_player.purse} Gold Coins."
-          advance_to_next_player
-        else
-          advance_to_next_player
-        end
-      else
+      unless current_player.in_penalty_box
         puts "Answer was correct!!!!"
-        current_player.purse += 1
+        current_player.gets_money
         puts "#{current_player.name} now has #{current_player.purse} Gold Coins."
-        advance_to_next_player
       end
+      advance_to_next_player
       !winner?
     end
 
     def wrong_answer
       puts "Question was incorrectly answered"
       puts "#{current_player.name} was sent to the penalty box"
-      current_player.in_penalty_box = true
+      current_player.enter_penalty_box
       advance_to_next_player
       !winner?
     end
 
     private
 
+    def check_if_exiting_penalty_box(roll)
+      return unless current_player.in_penalty_box
+
+      if roll.even?
+        puts "#{current_player.name} is not getting out of the penalty box"
+      else
+        puts "#{current_player.name} is getting out of the penalty box"
+        player.leave_penalty_box
+      end
+    end
+
     def advance_to_next_player
       players.rotate!
     end
 
     def ask_question
-      puts "#{current_category} Question #{questions[current_category]}"
-      questions[current_category] += 1
+      puts questions.next_question(current_category)
     end
 
     def current_category
-      QUESTION_CATEGORIES[current_player.place % 4]
+      Questions::CATEGORIES[current_player.place % 4]
     end
 
     def winner?
